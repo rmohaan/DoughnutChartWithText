1c265489073d316f1f9d3f62e328bc84eb792bf9
rmohaan <mohaan.raja@gmail.com>
2020-08-15 08:12:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import Pie from "./Pie";
+import "./styles.css";
+
+export default function App() {
+  useEffect(() => {});
+
+  let data = [5, 12, 8, 3, 10],
+    subTitle1 = "Money Spent",
+    subTitle2 = "0",
+    currency = "\u20B9",
+    showLabel = false,
+    colors = ["#43A19E", "#7B43A1", "#F2317A", "#FF9824", "#58CF6C"];
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <div>
+        <Pie
+          data={data}
+          subTitle1={subTitle1}
+          subTitle2={subTitle2}
+          radius={180}
+          hole={15}
+          colors={colors}
+          stroke={1}
+          strokeWidth={10}
+          showLabel={showLabel}
+          currency={currency}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/SliceC.js
@@ -0,0 +1,163 @@
+import React, { useEffect, useState, useCallback } from "react";
+import SubTitle from "./SubTitle";
+
+function getAnglePoint(startAngle, endAngle, radius, x, y) {
+  var x1, y1, x2, y2;
+
+  x1 = x + radius * Math.cos((Math.PI * startAngle) / 180);
+  y1 = y + radius * Math.sin((Math.PI * startAngle) / 180);
+  x2 = x + radius * Math.cos((Math.PI * endAngle) / 180);
+  y2 = y + radius * Math.sin((Math.PI * endAngle) / 180);
+
+  return { x1, y1, x2, y2 };
+}
+
+const SliceC = ({
+  fill,
+  stroke,
+  strokeWidth,
+  showLabel,
+  percent,
+  percentValue,
+  value,
+  ...props
+}) => {
+  const [path, setPath] = useState("");
+  const [pathAry, setPathArr] = useState("");
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [isMounted, setMounted] = useState(false);
+
+  const draw = useCallback(
+    (s) => {
+      if (isMounted) {
+        return;
+      }
+
+      var p = props,
+        pathArr = [],
+        a,
+        b,
+        c,
+        step;
+
+      step = p.angle / (37.5 / 2);
+
+      if (s + step > p.angle) {
+        s = p.angle;
+      }
+
+      // Get angle points
+      a = getAnglePoint(
+        p.startAngle,
+        p.startAngle + s,
+        p.radius,
+        p.radius,
+        p.radius
+      );
+      b = getAnglePoint(
+        p.startAngle,
+        p.startAngle + s,
+        p.radius - p.hole,
+        p.radius,
+        p.radius
+      );
+
+      pathArr.push("M" + a.x1 + "," + a.y1);
+      pathArr.push(
+        "A" +
+          p.radius +
+          "," +
+          p.radius +
+          " 0 " +
+          (s > 180 ? 1 : 0) +
+          ",1 " +
+          a.x2 +
+          "," +
+          a.y2
+      );
+      pathArr.push("L" + b.x2 + "," + b.y2);
+      pathArr.push(
+        "A" +
+          (p.radius - p.hole) +
+          "," +
+          (p.radius - p.hole) +
+          " 0 " +
+          (s > 180 ? 1 : 0) +
+          ",0 " +
+          b.x1 +
+          "," +
+          b.y1
+      );
+
+      // Close
+      pathArr.push("Z");
+
+      setPathArr(pathArr.join(" "));
+
+      if (s < p.angle) {
+        setTimeout(function () {
+          draw(s + step);
+        }, 16);
+      } else if (p.showLabel) {
+        c = getAnglePoint(
+          p.startAngle,
+          p.startAngle + p.angle / 2,
+          p.radius / 2 + p.trueHole / 2,
+          p.radius,
+          p.radius
+        );
+
+        setX(c.x2);
+        setY(c.y2);
+      }
+    },
+    [props, isMounted]
+  );
+
+  const animate = useCallback(() => {
+    draw(0);
+  }, [draw]);
+
+  useEffect(() => {
+    setMounted(true);
+    animate();
+    return () => {
+      setMounted(false);
+    };
+  }, [animate]);
+
+  useEffect(() => {
+    // console.log("hit", path);
+    setPath("");
+    console.log("empty path");
+  }, [props.radius, props.hole]);
+
+  useEffect(() => {
+    console.log("hit", path, pathAry);
+    animate();
+    setPath(pathAry);
+  }, [path, animate]);
+
+  // console.log( "path", path, "\n", pathAry);
+  return (
+    <g overflow="hidden">
+      <path
+        d={path}
+        fill={fill}
+        stroke={stroke}
+        strokeWidth={strokeWidth ? strokeWidth : 3}
+      />
+      {showLabel && percentValue > 5 ? (
+        <text x={x} y={y} fill="#fff" textAnchor="middle">
+          {percent ? percentValue + "%" : value}
+        </text>
+      ) : null}
+      {props.subTitle1 ? (
+        <SubTitle {...props} textAnchor="middle" fill="#aaa" />
+      ) : null}
+    </g>
+  );
+};
+
+export default SliceC;
